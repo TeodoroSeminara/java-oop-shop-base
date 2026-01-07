7acb256cb964a28f026f24439205294145702322
Teodoro <seminara.teodoro93@gmail.com>
2026-01-07 18:00:20 +0100

    OOP part2

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
+        /* Prima versione esercizio
+                Scanner sc = new Scanner(System.in);
 
         System.out.print("Inserisci il nome del prodotto: ");
         String nomeProdotto = sc.nextLine();
@@ -22,5 +24,45 @@ public class Main {
         prodotto1.prezzoBase();
         prodotto1.prezzoIva();
         prodotto1.nomeEsteso();
+        */
+
+        Prodotto prodotto1 = new Prodotto("Solo nome");
+        Prodotto prodotto2 = new Prodotto("Nome", "Descizione", 10);
+
+        System.out.println(prodotto1.getNome());
+        prodotto1.setNome("Cambiato nome al prodotto 1");
+        System.out.println("Il codice del prodotto è: " + prodotto1.getCodice());
+        System.out.println("Il nome del prodotto è: " + prodotto1.getNome());
+        System.out.println("La descrizione è: " + prodotto1.getDescrizione());
+        System.out.println("Il prezzo base è: " + prodotto1.getPrezzoBase());
+        prodotto1.getNomeEsteso();
+        prodotto1.getPrezzoIva();
+        prodotto1.setDescrizione("La descizione è stata aggiunta");
+        prodotto1.setPrezzoBase(15);
+        System.out.println("La descrizione aggiornata è: " + prodotto1.getDescrizione());
+        System.out.println("Il prezzo base aggiornato è: " + prodotto1.getPrezzoBase());
+        prodotto1.getNomeEsteso();
+        prodotto1.getPrezzoIva();
+
+
+
+
+
+
+        System.out.println(prodotto2.getNome());
+        prodotto2.setNome("Diverso");
+        System.out.println("Il codice del prodotto è: " + prodotto2.getCodice());
+        System.out.println("Il nome del prodotto è: " + prodotto2.getNome());
+        System.out.println("La descrizione è: " + prodotto2.getDescrizione());
+        System.out.println("Il prezzo base è: " + prodotto2.getPrezzoBase());
+        prodotto2.getNomeEsteso();
+        prodotto2.getPrezzoIva();
+        prodotto2.setDescrizione("La descizione è stata aggiunta");
+        prodotto2.setPrezzoBase(15);
+        System.out.println("La descrizione aggiornata è: " + prodotto2.getDescrizione());
+        System.out.println("Il prezzo base aggiornato è: " + prodotto2.getPrezzoBase());
+        prodotto2.getNomeEsteso();
+        prodotto2.getPrezzoIva();
+
     }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -11,13 +11,20 @@ public class Prodotto {
     public double prezzo;
     public double iva;
 
-    public Prodotto( String nome, String descrizione, double prezzo) {
+    public Prodotto(String nome, String descrizione, double prezzo) {
         this.codice = codiceRandom();
         this.nome = nome;
         this.descrizione = descrizione;
         this.prezzo = prezzo;
-        iva= prezzo + (prezzo * 22 /100);
+        iva = prezzo + (prezzo * 22 / 100);
         // Correzione: this.iva = iva ( dichiarata come bigDecimal)
+    }
+
+    public Prodotto(String nome) {
+        this.codice = codiceRandom();
+        this.nome = nome;
+        this.descrizione = "L'articolo non ha descrizione";
+        this.prezzo = 0;
     }
 
     /* Correzione:
@@ -38,21 +45,73 @@ public class Prodotto {
     }
     */
 
-
-    public int codiceRandom(){
+    private int codiceRandom() {
         Random r = new Random();
         return codice = r.nextInt(1100, 2999);
     }
 
-    public void prezzoBase(){
+    public int getCodice() {
+        return this.codice;
+    }
+
+//    public void prezzoBase(){
+//        System.out.println("Il prezzo base dell'articolo " + nome + " è: " + prezzo + "€");
+//    }
+
+//    public void prezzoIva(){
+//        System.out.println("Il prezzo con Iva all 22% dell'articolo " + nome + " è: " + iva + "€");
+//    }
+//
+//    public void nomeEsteso(){
+//        System.out.println(nome + "-" + codice);
+//    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        if (nome.isEmpty() || nome.equals(" ")) {
+            System.out.println("Il nome inserito non è valido");
+        } else {
+            this.nome = nome;
+        }
+    }
+
+    public String getDescrizione() {
+        return this.descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        if (descrizione.isEmpty() || descrizione.equals(" ")) {
+            System.out.println("La descrizione è vuota");
+        } else {
+            this.descrizione = descrizione;
+        }
+    }
+
+    public double getPrezzoBase() {
         System.out.println("Il prezzo base dell'articolo " + nome + " è: " + prezzo + "€");
+        return this.prezzo;
     }
 
-    public void prezzoIva(){
-        System.out.println("Il prezzo con Iva all 22% dell'articolo " + nome + " è: " + iva + "€");
+    public void setPrezzoBase(double prezzo) {
+        if (prezzo > 0) {
+            this.prezzo = prezzo;
+        } else {
+            System.out.println("Il prezzo deve essere superiore a 0€");
+        }
     }
 
-    public void nomeEsteso(){
+    public void getPrezzoIva() {
+        if (this.prezzo == 0){
+            System.out.println("L'iva non può essere calcolata");
+        } else  {
+            System.out.println("Il prezzo con Iva al 22% dell'articolo " + nome + " è: " + iva + "€");
+        }
+    }
+
+    public void getNomeEsteso() {
         System.out.println(nome + "-" + codice);
     }
 }
